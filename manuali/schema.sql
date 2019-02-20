78270478ad397d29d0abd79fa589fa271ed79329
Jacopo <sj.vergaror@mail.scuole.vda.it>
2019-02-20 09:51:08 +0000

    Modifiche schema.sql

--- manuali/schema.sql
+++ manuali/schema.sql
@@ -6,19 +6,14 @@
 --
 -- Using output format: mysql
 
-
-
+-- PER FARE PULIZIA. occhio!
+-- DROP TABLE esempi; DROP TABLE comandi; DROP TABLE argomenti; 
 CREATE TABLE argomenti (
       id_argomento     int PRIMARY KEY AUTO_INCREMENT,
       nome     varchar(255),
       descrizione     text,
       id_padre    int NOT NULL
 );
-
-
-
-
-
 
 CREATE TABLE comandi (
       id_comando     int PRIMARY KEY AUTO_INCREMENT,
@@ -27,11 +22,6 @@ CREATE TABLE comandi (
       sintassi     text,
       id_argomento     int NOT NULL
 );
-
-
-
-
-
 
 CREATE TABLE esempi (
       id     int PRIMARY KEY AUTO_INCREMENT,
@@ -43,8 +33,14 @@ CREATE TABLE esempi (
 
 
 
+INSERT into argomenti  (nome, descrizione, id_padre) VALUES ('root','Root dei manuali',1);
+-- INSERT into argomenti  (id_argomento, nome, descrizione, id_padre) VALUES (0,'root','Root dei manuali',0); -- non funziona ma non chiaro (approfondire)
+ALTER TABLE argomenti ADD FOREIGN KEY (id_padre) REFERENCES argomenti (id_argomento); 
 
-ALTER TABLE argomenti ADD FOREIGN KEY (id_padre) REFERENCES argomenti (id_argomento);
+INSERT into argomenti  (nome, descrizione, id_padre) VALUES ('Informatica','Argomenti vari di informatica 5bit as 2018-2019',1);
+INSERT into argomenti  (nome, descrizione, id_padre) VALUES ('Sistemi e reti','Argomenti vari di SER 5bit as 2018-2019',1);
+INSERT into argomenti  (nome, descrizione, id_padre) VALUES ('SQL','Linguaggio database',2);
+SELECT * FROM argomenti;
 
 
 ALTER TABLE comandi ADD FOREIGN KEY (id_argomento) REFERENCES argomenti (id_argomento);
